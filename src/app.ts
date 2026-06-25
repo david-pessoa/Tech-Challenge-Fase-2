0301d7b231b1276de0f47b1ff73ef508b89c6ef5
MichelecFernandes <michelecristinafr@gmail.com>
2026-06-24 22:40:30 -0300

    feat: registrar rotas de posts na aplicacao

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
+import { postRoutes } from "./routes/post.routes";
+
 export const app = express();
 
 const swaggerDocument = YAML.load('./src/docs/openapi.yaml');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/posts", postRoutes);
 
 app.use(express.json());
