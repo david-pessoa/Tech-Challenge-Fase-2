7413140d4e31217d096132cff195ae84e0373193
MichelecFernandes <michelecristinafr@gmail.com>
2026-06-25 21:51:23 -0300

    feat: implementar busca de post por id

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -4,5 +4,6 @@ import { postController } from "../controllers/PostController";
 const router = Router();
 
 router.post("/", postController.create);
+router.get("/:id", postController.findById);
 
 export { router as postRoutes };
---
+++ src/services/post/GetPostService.ts
@@ -0,0 +1,28 @@
+import { postRepository } from "../../repositories/PostRepository";
+
+export class GetPostService {
+  async execute(id: string) {
+    const post = await postRepository.findOne({
+      where: { id },
+      relations: {
+        user: true,
+      },
+    });
+
+    if (!post) {
+      throw new Error("Post não encontrado");
+    }
+
+    return {
+      postId: post.id,
+      userId: post.user.id,
+      titulo: post.titulo,
+      descricao: post.descricao,
+      conteudo: post.conteudo,
+      dataCriacao: post.dataCriacao,
+      dataModificacao: post.dataModificacao,
+    };
+  }
+}
+
+export const getPostService = new GetPostService();
