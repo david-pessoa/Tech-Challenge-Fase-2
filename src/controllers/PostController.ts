e2020a495b92640fdf79dbc2f3ad1ff344d9f923
MichelecFernandes <michelecristinafr@gmail.com>
2026-06-25 21:09:08 -0300

    fix: ajuste de retorno da criacao de posts para seguir documentacao do swagger

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -3,9 +3,17 @@ import { createPostService } from "../services/post/CreatePostService";
 
 export class PostController {
   async create(request: Request, response: Response) {
-    const newPost = await createPostService.execute(request.body);
+    try {
+      await createPostService.execute(request.body);
 
-    return response.status(201).json(newPost);
+      return response.status(201).json({
+        message: "Post criado com sucesso!",
+      });
+    } catch (error) {
+      return response.status(400).json({
+        message: "Dados inválidos",
+      });
+    }
   }
 }
 
